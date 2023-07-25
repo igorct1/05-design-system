269b820a3d71da6a64259db0003876bbaba27eae
igoreason <igorct77@gmail.com>
2023-07-25 20:19:03 -0300

    feat: tokens & tsup setup

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,10 @@
+export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
+
