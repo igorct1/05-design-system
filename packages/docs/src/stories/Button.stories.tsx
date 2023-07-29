fd897adcce16715f2d544e5717c29ed673802254
igoreason <igorct77@gmail.com>
2023-07-29 09:24:09 -0300

    feat: external fontfamily

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,17 +1,16 @@
-import type { StoryObj, Meta } from '@storybook/react'
-import { Button } from '@reason-ignite-ui/react'
-
+import { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@reason-ignite-ui/react'
 export default {
   title: 'Button',
   component: Button,
+
   args: {
     children: 'Enviar',
   },
-} as Meta
+} as Meta<ButtonProps>
 
-export const Primary: StoryObj = {}
-
-export const Big: StoryObj = {
+export const Primary: StoryObj<ButtonProps> = {}
+export const Big: StoryObj<ButtonProps> = {
   args: {
     size: 'big',
   },
