9882d34eba41ada41a9dbbd9853dd04253637576
igoreason <igorct77@gmail.com>
2023-07-30 14:32:30 -0300

    feat: textinput stories component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,27 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { TextInput, TextInputProps } from '@reason-ignite-ui/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+
+  args: {},
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Text your name...',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
